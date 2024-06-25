8a9191a06a4069c93c3084e275487020578a7afc
Lokeshkumar Veshala <v.lokeshkumar9@gmail.com>
2024-06-25 16:13:59 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "instance_count" {
+  description = "Number of instances"
+  default     = 1
+}
+
+variable "environment" {
+  description = "Environment name"
+  default     = "development"
+}
